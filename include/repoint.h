0134452aa02b9bb7083f793c2cac0903191037cd
Neirn <neirny@gmail.com>
2025-05-28 18:22:38 -0700

    Create repoint.h

---
+++ include/repoint.h
@@ -0,0 +1,13 @@
+#ifndef __REPOINT_H__
+#define __REPOINT_H__
+#include "global.h"
+
+void ZGlobalObj_globalizeGfx(void *obj, Gfx *segmentedPtr);
+
+void ZGlobalObj_globalizeDL(void *obj, Gfx *segmentedPtr);
+
+void ZGlobalObj_globalizeLodLimbSkeleton(void *obj, FlexSkeletonHeader *skel);
+
+void ZGlobalObj_globalizeStandardLimbSkeleton(void *obj, FlexSkeletonHeader *skel);
+
+#endif
